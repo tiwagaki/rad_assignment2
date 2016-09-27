54b4f75009a44498c28a97e12376a94de0b819ee
tiwagaki <takuya.iwagaki@openlink.com>
2016-09-27 21:00:41 +0800

    27Sep_UIUpdates

--- taxidimanasg/main.py
+++ taxidimanasg/main.py
@@ -23,10 +23,9 @@ import jinja2
 import os
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions = ['jinja2.ext.autoescape'],
-    autoescape = True)
-
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')),
+    extensions=['jinja2.ext.autoescape'])
+	
 url = 'http://datamall2.mytransport.sg/ltaodataservice/Taxi-Availability'
 
 headers = { 'AccountKey' : 'jmh3bT4mS0awdFDL6G6pSA==',
